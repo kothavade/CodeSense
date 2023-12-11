bc649d4aa079a094ec7113af814af3508a980be5
Ved Kothavade <ved@kothavade.com>
2023-12-10 20:59:22 -0500

    update nix; add source link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center flex-grow">
         <h2 className="text-2xl mb-8">Welcome to CodeSense!</h2>
         <div className="flex flex-row gap-x-4 mb-8">
-          <Link href="/repo" className={buttonVariants({ variant: "default" })}  >
+          <Link href="/repo" className={buttonVariants({ variant: "default" })}>
             Have a github repo?
           </Link>
           <Link
@@ -27,6 +27,9 @@ export default function Home() {
 
       <footer className="mt-12 text-center">
         <p>&copy; {new Date().getFullYear()} CodeSense. All rights reserved.</p>
+        <Link href="https://github.com/kothavade/CodeSense/tree/rewrite">
+          <a className="underline text-blue-500">View Source Code</a>
+        </Link>
       </footer>
     </div>
   );
